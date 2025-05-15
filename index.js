9c389d96f10d31bb5e83273b72cfb7ab6414944a
DanielSajvin <msajvingomez@gmail.com>
2025-05-15 11:23:39 -0600

    Deploy con base de datos

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// index.js
 const express = require("express");
 const app = express();
+const connection = require("./db");
+require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("¡Hola desde Railway!");
+app.get("/usuarios", (req, res) => {
+  connection.query("SELECT * FROM usuarios", (err, results) => {
+    if (err) {
+      console.error("Error al obtener usuarios:", err);
+      res.status(500).send("Error en el servidor");
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
